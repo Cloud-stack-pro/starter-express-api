e3c23c33f70513ba587b36808b2823a3aa51da04
Cloud code <110151168+Cloud-stack-pro@users.noreply.github.com>
2023-01-14 18:18:42 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,115 @@
+const { getDatabase, ref, get, set, remove } = require("firebase/database");
+const TokenGenerator = require("uuid-token-generator");
+const { initializeApp } = require("firebase/app");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const client = express();
+
+client.use( cors() );
+client.set( "view engine", "ejs" );
+client.use( express.json({limit:'1gb'}) );
+client.use( express.urlencoded({ extended: true }) );
+client.set( "views", path.join( __dirname, "public" ) );
+
+/**
+ * @param { String } databaseURL
+ * @return { void }
+ **/
+function firebase( databaseURL ) {
+    const application = initializeApp({
+        databaseURL: databaseURL
+    });
+    return getDatabase( application );
+}
+firebase.exists = async function( databaseURL ) {
+    try {
+        const database = ref( firebase( databaseURL ), "testing" );
+        await get( database )
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
+const uid = new TokenGenerator(512, TokenGenerator.BASE62);
+
+// make new
+client.post("/api/v1/server", async ( req, res ) => {
+    const isFirebase = await firebase.exists( req.body['databaseURL'] || "https://google.com" );
+    if ( !isFirebase || !req.body['databaseURL'] || !req.body['databasePATH'] || !req.body['state'] ) return res.status(404).render("error");
+    
+    if ( req.body['state']?.toUpperCase() == "GET" ) {
+        const initialize = new Array();
+        const database = ref( firebase( req.body['databaseURL'] ), req.body['databasePATH'] );
+        await get( database ).then( snapshots => {
+            snapshots.forEach( snapshot => {
+                initialize.push( snapshot.val() )
+            })
+            res.status(200).json({
+                statusCode: 200,
+                response: initialize
+            })
+        }).catch( error => {
+            res.status(404).json({
+                statusCode: 404,
+                response: initialize
+            })
+        })
+        return !0
+    } else if ( req.body['state']?.toUpperCase() == "SET" && typeof req.body['data'] == "object" ) {
+        let token = uid.generate();
+        if ( !req.body['data'].token ) {
+            req.body['data'].token = token;
+        } else {
+            token = req.body['data'].token;
+        }
+        const database = ref( firebase( req.body['databaseURL'] ), req.body['databasePATH'] + `/${token}` );
+        await set( database, req.body['data'] ).then( snapshots => {
+            res.status(200).json({
+                statusCode: 200,
+                success: true
+            })
+        }).catch( error => {
+            res.status(404).json({
+                statusCode: 404,
+                success: false
+            })
+        })
+        return !0
+    } else if ( req.body['state']?.toUpperCase() == "DEL" && typeof req.body['remove'] == "object" ) {
+        let fullpath = req.body['databasePATH'];
+        if ( req.body['remove'].token ) {
+            fullpath += `/${req.body['remove'].token}`;
+        }
+        const database = ref( firebase( req.body['databaseURL'] ), fullpath );
+        await remove( database ).then( snapshots => {
+            res.status(200).json({
+                statusCode: 200,
+                remove: true
+            })
+        }).catch( error => {
+            res.status(404).json({
+                statusCode: 404,
+                remove: false
+            })
+        })
+        return !0
+    } else {
+        return res.status(404).render("error")
+    }
+});
+
+client.get("*", async ( req, res ) => {
+    res.status(404).render("error");
+});
+client.all("*", async ( req, res ) => {
+    res.status(404).json({
+        statusCode: 404,
+        message: "File not found!"
+    });
+});
+
+client.listen(8250, function() {
+    console.log(" [INFO] Server running " + new Date().toUTCString() )
+})
